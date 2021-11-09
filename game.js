b018207864708856440209ab7781d0a51d6c262a
Gabriel Issa <issag2016@gmail.com>
2021-11-09 14:54:41 -0500

    gabe

--- game.js
+++ game.js
@@ -24,11 +24,11 @@ laserImg.onload=()=>{
 
 
 class Laser {
-    constructor(top,){
+    constructor(top,height){
         this.x = canvas.width,
         this.y = 0,
         this.w = 100,
-        this.h = Math.random()*300
+        this.h = height
         this.image= laserImg
         this.top= top
         
@@ -60,6 +60,10 @@ window.onkeydown = function (e) {
         case 'ArrowUp':
         megaMan.y -= 50;
             break;
+
+        case 'ArrowDown':
+        megaMan.y+= 50;
+        break;
     }
 }
 
@@ -67,12 +71,19 @@ const lasers = []
 
 //Spawing enemies in random place aka adding enemy objects to enemy array
 setInterval(() => {
+    let gap= 100
+    let height=600
+    let topLaserHeight=Math.random()*300
+    let bottomLaserHeight= height-topLaserHeight-gap
     console.log("add a new laser", lasers)
-    lasers.push(new Laser(true))
-    lasers.push(new Laser(false))
+    lasers.push(new Laser(true,topLaserHeight))
+    lasers.push(new Laser(false,bottomLaserHeight))
 }, 5000)
 
-
+let score=0
+setInterval(() => {
+    score += 10
+}, 1000)
 
 
 
@@ -82,115 +93,30 @@ int=window.requestAnimationFrame(animate)
 
 ctx.clearRect(0,0,canvas.width,canvas.height)
 
-
+document.querySelector('p span').innerText = score
 
 
 for (let laser of lasers){
     ctx.drawImage(laser.image,laser.x--,(laser.top?laser.y:canvas.height-laser.h),laser.w,laser.h)
-    //ctx.drawImage(laser.image,laser.x--,laser.y,laser.w,laser.h)
-    // detectCollision(car, roadblock)
+    detectCollision(megaMan, laser)
 }
 
 ctx.drawImage(megaImg, megaMan.x, megaMan.y, megaMan.w, megaMan.h)
-//ctx.drawImage(laserImg, laserBeam.x, laserBeam.y, laserBeam.w, laserBeam.h)
+
 
 // document.querySelector('body p').innerHTML = pointcounter
 }
 
 animate()
 
-
-//CODE EXAMPLE
-
-// var cvs = document.getElementById("canvas");  
-// var ctx = cvs.getContext("2d");  
-
-// var bird = new Image();  
-// var bg = new Image();  
-// var fg = new Image();  
-// var pipeNorth = new Image();  
-// var pipeSouth = new Image();  
-  
-// bird.src = "images/pngegg.png";  
-// bg.src = "images/pngegg.png";  
-// fg.src = "images/pngegg.png";  
-// pipeNorth.src = "images/pngegg.png";  
-// pipeSouth.src = "images/pngegg.png";  
-  
-// var gap = 85;  
-// var constant;  
-  
-// var bX = 10;  
-// var bY = 150;  
-  
-// var gravity = 1.5;  
-  
-// var score = 0;  
-  
-// var fly = new Audio();  
-// var scor = new Audio();  
-  
-// fly.src = "sounds/fly.mp3";  
-// scor.src = "sounds/score.mp3";  
-  
-// document.addEventListener("keydown",moveUp);  
-  
-// function moveUp(){  
-//     bY -= 25;  
-//     fly.play();  
-// }  
-  
-// var pipe = [];  
-  
-// pipe[0] = {  
-//     x : cvs.width,  
-//     y : 0  
-// };  
-  
-// function draw(){  
-      
-//     ctx.drawImage(bg,0,0);  
-      
-      
-//     for(var i = 0; i < pipe.length; i++){  
-          
-//         constant = pipeNorth.height+gap;  
-//         ctx.drawImage(pipeNorth,pipe[i].x,pipe[i].y);  
-//         ctx.drawImage(pipeSouth,pipe[i].x,pipe[i].y+constant);  
-               
-//         pipe[i].x--;  
-          
-//         if( pipe[i].x == 125 ){  
-//             pipe.push({  
-//                 x : cvs.width,  
-//                 y : Math.floor(Math.random()*pipeNorth.height)-pipeNorth.height  
-//             });   
-//         }  
-          
-//         if( bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width && (bY <= pipe[i].y + pipeNorth.height || bY+bird.height >= pipe[i].y+constant) || bY + bird.height >=  cvs.height - fg.height){  
-//             location.reload();
-//         }  
-          
-//         if(pipe[i].x == 5){  
-//             score++;  
-//             scor.play();  
-//         }  
-          
-          
-//     }  
-  
-//     ctx.drawImage(fg,0,cvs.height - fg.height);  
-      
-//     ctx.drawImage(bird,bX,bY);  
-      
-//     bY += gravity;  
-      
-//     ctx.fillStyle = "#000";  
-//     ctx.font = "20px Verdana";  
-//     ctx.fillText("Score : "+score,10,cvs.height-20);  
-      
-//     requestAnimationFrame(draw);  
-      
-// }  
-  
-// draw();  
+function detectCollision(hero, columns) {
+    if (hero.x < columns.x + columns.w &&
+        hero.x + hero.w > columns.x &&
+        hero.y < columns.y + columns.h &&
+        hero.h + hero.y > columns.y) {
+        console.log('collision')
+        window.cancelAnimationFrame(int)
+        // window.location.reload()
+    }
+    // collision detected!
+  }
